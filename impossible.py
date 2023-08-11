3ed93705a1cfe4341d529d57be03a30369cf3dc1
James <james.ca@telus.net>
2023-08-10 20:56:52 -0700

    first time rendering impossible figure correctly

--- impossible.py
+++ impossible.py
@@ -1,61 +1,63 @@
 from PIL import Image
 import random
 import numpy as np
+import math
+
+def on_tile(xcoord , ycoord, vectorA, vectorB, box_list, multiplier):
 
 
-def on_tile(xcoord , ycoord, vectorA, vectorB, vectorC, cube_list):
-    #side = -1
-    #rel_pos = [-1,-1]
-    for cube_index in range(len(cube_list)):
-        trueY = ycoord + vectorA[0] * cube_list[cube_index][0] + vectorB[0] * cube_list[cube_index][1]
-        trueX = xcoord + vectorA[1] * cube_list[cube_index][0] + vectorB[1] * cube_list[cube_index][1]
+
         
-        posL = np.linalg.solve(((vectorA[0],vectorB[0]),(vectorA[1],vectorB[1])),(trueY,trueX))
-        posR = np.linalg.solve(((vectorA[0],vectorC[0]),(vectorA[1],vectorC[1])),(trueY,trueX))
-        posT = np.linalg.solve(((vectorC[0],vectorB[0]),(vectorC[1],vectorB[1])),(trueY,trueX))
+    posL = np.linalg.solve(((vectorA[0],vectorB[0]),(vectorA[1],vectorB[1])),(ycoord,xcoord))
 
+    xBox = math.floor(posL[1]) - multiplier[0] * math.floor(posL[0])
+    yBox = math.floor(posL[0]) - multiplier[1] * math.floor(posL[1])
 
-        if np.max(posL) <1 and np.min(posL) >= 0 :
-            return 0
-        elif np.max(posR) <1 and np.min(posR) >= 0 :
-            return 1
-        elif np.max(posT) <1 and np.min(posT) >= 0 :
-            return 2
+    if xBox >= 0 and xBox <= 4 and yBox >=0 and yBox <=4 :
+        return box_list[4 - yBox][xBox]
     
-    return -1
+    return 0
 
 
 # Create the array that determines where cubes are
 box_list = [[0]*5 for i in range(5)]
-box_count = 0
-for x in range(5):
-    for y in range(5):
-        if random.randint(0,1) == 1:
-            box_list[x][y] = 1
-            box_count += 1;
+##box_count = 0
+##for x in range(5):
+##    for y in range(5):
+##        if random.randint(0,1) == 1:
+##            box_list[x][y] = 1
+##            box_count += 1;
 
 
-##box_list = [[1,1,1,1,1],
-##            [1,1,0,0,1],
-##            [1,0,0,0,1],
-##            [1,1,0,0,1],
-##            [1,1,1,1,1]]
-box_count = 18
-cube_list = [[0]*2 for i in range(box_count)]
-box_index = 0
-for x in range(5):
-    for y in range(5):
-        if box_list[x][y] == 1:
-            cube_list[box_count - box_index - 1][0] = x
-            cube_list[box_count - box_index - 1][1]= y
-            box_index +=1
 
-##cube_list = [[0] *2 for i in range(15)]
-##cube_list[1] = [0,4]
-##cube_list[2] = [0,3]
-##cube_list[3] = [0,2]
-##cube_list[4] = [0,1]
-##cube_list[5] = [0,0]
+##box_count = 18
+##cube_list = [[0]*2 for i in range(box_count)]
+##box_index = 0
+##for x in range(5):
+##    for y in range(5):
+##        if box_list[x][y] == 1:
+##            cube_list[box_count - box_index - 1][0] = x
+##            cube_list[box_count - box_index - 1][1]= y
+##            box_index +=1
+
+Lbox_list = [[1,2,1,1,0],
+            [1,0,0,0,0],
+            [1,0,0,0,0],
+            [1,0,0,0,0],
+            [1,0,0,0,0]]
+
+Rbox_list = [[0,0,0,0,1],
+            [1,0,0,1,0],
+            [1,0,1,0,0],
+            [1,1,0,0,0],
+            [1,0,0,0,0]]
+
+Tbox_list = [[1,1,1,1,1],
+            [0,0,0,1,0],
+            [0,0,1,0,0],
+            [0,1,0,0,0],
+            [0,0,0,0,0]]
+
 
 
 
@@ -63,35 +65,38 @@ for x in range(5):
 image_width = 100;
 image_height = 100;
 
-my_array = np.full((image_width,image_height,3),(0,0,0), dtype=np.ubyte)
+my_array = np.full((image_height,image_width,3),(0,0,0), dtype=np.ubyte)
 
 
-vc1 = (0,-1)
-vc2 = (-1,0.5)
-vc3 = (1,0.5)
+Avc1 = (0,1)
+Avc2 = (1,0.2)
 
-image_magnitude = 3/(image_height+image_width/2)
+Bvc1 = (0,1)
+Bvc2 = (1,1.2)
 
-offset = (-image_width *3/5 , -image_height/5)
-
+Cvc1 = (1, 1.2)
+Cvc2 = (1, 0.2)
 
 
 print("beginning cubes")
 
-for xind in range(image_width):
-    for yind in range(image_height):
-        xcoord = (-xind - offset[0]) * image_magnitude * 3.5
-        ycoord = (yind + offset[1]) * image_magnitude  * 3.5
+for xind in range(image_height):
+    for yind in range(image_width):
+        xcoord = (image_height - xind ) / image_height * 9 -1
+        ycoord = (yind ) / image_width * 9 -1
 
 
-        side = on_tile(xcoord, ycoord, vc1 , vc2, vc3,cube_list)
+        check = on_tile(xcoord, ycoord, Avc1 , Avc2,Lbox_list, (0,0))
 
-        if side == 0:
+
+        if check >0 :
             my_array[xind][yind] = (255,255,255)
-        elif side == 1 :
-            my_array[xind][yind] = (0,0,255)
-        elif side == 2 :
+        if check !=2 and   on_tile(xcoord - 1, ycoord , Cvc1 , Cvc2,Tbox_list, (-1,0)):
             my_array[xind][yind] = (0,255,255)
+        
+        if check !=2 and   on_tile(xcoord - 0.2, ycoord - 1, Bvc1 , Bvc2,Rbox_list, (0,-1)):
+            my_array[xind][yind] = (0,0,255)
+        
         
 
 
