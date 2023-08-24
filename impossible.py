aa6e29de84184ba3e9509bdfa7741064c99b7850
James <james.ca@telus.net>
2023-08-24 02:40:24 -0700

    added textures and began what will be the early interface

--- impossible.py
+++ impossible.py
@@ -1,7 +1,9 @@
-from PIL import Image
+from PIL import Image, ImageTk
 import random
 import numpy as np
 import math
+from texture import texture
+import tkinter as tk
 
 def on_tile(xcoord , ycoord, vectorA, vectorB, box_list, multiplier):
 
@@ -13,63 +15,74 @@ def on_tile(xcoord , ycoord, vectorA, vectorB, box_list, multiplier):
     xBox = math.floor(posL[1]) - multiplier[0] * math.floor(posL[0])
     yBox = math.floor(posL[0]) - multiplier[1] * math.floor(posL[1])
 
-    if xBox >= 0 and xBox <= 4 and yBox >=0 and yBox <=4 :
-        return box_list[4 - yBox][xBox]
+    if xBox >= 0 and xBox < len(box_list[0]) and yBox >=0 and yBox <len(box_list) :
+        return box_list[len(box_list) - 1 - yBox][xBox], posL[0]%1 , posL[1]%1
     
-    return 0
+    return 0 ,0 , 0
+
+def find_image_bounds(vector1, vector2, boxShape):
+    right = max(0, vector1[0],vector2[0], vector1[0] + vector2[0]) * boxShape[0]
+    left = min(0, vector1[0],vector2[0], vector1[0] + vector2[0]) * boxShape[0] 
+
+    top = max(0, vector1[1],vector2[1], vector1[1] + vector2[1]) * boxShape[1]
+    bottom = min(0, vector1[1],vector2[1], vector1[1] + vector2[1]) * boxShape[1]
+
+    xmultiplier = top - bottom 
+    ymultiplier = right - left 
+
+    multiplier = max(xmultiplier, ymultiplier)+1
+    return multiplier, bottom - 0.5*( multiplier - xmultiplier), left -0.5* (multiplier - ymultiplier)
 
 
 # Create the array that determines where cubes are
 box_list = [[0]*5 for i in range(5)]
-##box_count = 0
-##for x in range(5):
-##    for y in range(5):
-##        if random.randint(0,1) == 1:
-##            box_list[x][y] = 1
-##            box_count += 1;
-
-
-
-##box_count = 18
-##cube_list = [[0]*2 for i in range(box_count)]
-##box_index = 0
-##for x in range(5):
-##    for y in range(5):
-##        if box_list[x][y] == 1:
-##            cube_list[box_count - box_index - 1][0] = x
-##            cube_list[box_count - box_index - 1][1]= y
-##            box_index +=1
-
-Lbox_list = [[1,2,1,1,0],
-            [1,0,0,0,1],
-            [1,0,0,0,1],
-            [1,0,0,0,2],
-            [1,1,1,1,1]]
-
-Rbox_list = [[0,0,0,0,1],
-            [1,0,0,1,1],
-            [1,0,1,0,1],
-            [1,1,0,0,1],
-            [0,0,0,0,1]]
-
-Tbox_list = [[1,1,1,1,1],
-            [0,0,0,1,0],
-            [0,0,1,0,0],
-            [0,1,0,0,0],
-            [0,2,1,1,0]]
 
 
 
 
 
-image_width = 100;
-image_height = 100;
+
+Lbox_list =[[0,0,0,0,0,0],
+            [0,0,0,0,1,0],
+            [0,0,1,0,1,0],
+            [0,0,2,0,2,0],
+            [0,0,0,0,0,0],
+            [1,1,1,1,1,0]]
+
+Rbox_list =[[0,0,0,0,1,0],
+            [0,0,0,1,1,0],
+            [0,0,1,0,1,0],
+            [0,0,1,0,1,0],
+            [0,0,0,0,0,1],
+            [0,0,0,0,1,0]]
+
+Tbox_list =[[0,0,0,0,1,0],
+            [0,0,0,1,0,0],
+            [0,0,1,0,0,0],
+            [0,0,0,0,0,0],
+            [0,1,0,1,0,1],
+            [1,1,1,1,1,0]]
+
+
+
+top_texture = texture((0,255,255))
+left_texture = texture((255,255,255))
+right_texture = texture((0,0,255))
+
+top_texture.setTexture("brick.jpg")
+left_texture.setTexture("portal.jpg")
+right_texture.setTexture("test.jpg")
+
+
+
+image_width = 500;
+image_height = 500;
 
 my_array = np.full((image_height,image_width,3),(0,0,0), dtype=np.ubyte)
 
 
-Avc1 = (0,1)
-Avc2 = (1,-0.5)
+Avc1 = (0,1) # vertical vector component
+Avc2 = (0.87,-0.5) # horizontal vector component
 
 Bvc1 = Avc1
 Bvc2 = (Avc1[0] + Avc2[0], Avc1[1] + Avc2[1])
@@ -80,29 +93,44 @@ Cvc2 = Avc2
 
 print("beginning cubes")
 
+xmultiplier , xoffset, yoffset = find_image_bounds(Avc1,Avc2,(len(Lbox_list[0]),len(Lbox_list)))
+ymultiplier = xmultiplier
+
 for xind in range(image_height):
     for yind in range(image_width):
-        xcoord = (image_height - xind ) / image_height * 9 -3
-        ycoord = (yind ) / image_width * 9 -1
+        xcoord = (image_height - xind ) / image_height * xmultiplier + xoffset 
+        ycoord = (yind ) / image_width * ymultiplier  + yoffset 
 
 
-        check = on_tile(xcoord, ycoord, Avc1 , Avc2,Lbox_list, (0,0))
+        check, xTilePos, yTilePos = on_tile(xcoord, ycoord, Avc1 , Avc2,Lbox_list, (0,0))
 
 
         if check >0 :
-            my_array[xind][yind] = (255,255,255)
+            my_array[xind][yind] = left_texture.getColour(xTilePos,yTilePos)
         if check !=2:
-            check = on_tile(xcoord - Avc1[1], ycoord - Avc1[0] , Cvc1 , Cvc2,Tbox_list, (-1,0))
+            check, xTilePos, yTilePos = on_tile(xcoord - Avc1[1], ycoord - Avc1[0] , Cvc1 , Cvc2,Tbox_list, (-1,0))
             if check >0 :
-                my_array[xind][yind] = (0,255,255)
+                my_array[xind][yind] = top_texture.getColour(xTilePos,yTilePos)
         
-        if check !=2 and   on_tile(xcoord - Avc2[1], ycoord - Avc2[0], Bvc1 , Bvc2,Rbox_list, (0,-1)):
-            my_array[xind][yind] = (0,0,255)
-        
+        if check !=2 :
+            check, xTilePos, yTilePos = on_tile(xcoord - Avc2[1], ycoord - Avc2[0], Bvc1 , Bvc2,Rbox_list, (0,-1))
+            if check >0 :
+                my_array[xind][yind] = right_texture.getColour(xTilePos,yTilePos)        
         
 
 
+window = tk.Tk()
 
 im = Image.fromarray(my_array)
+tkimage = ImageTk.PhotoImage(im)
 
-im.show()
+label = tk.Label(window , image = tkimage)
+
+label.pack()
+
+window.mainloop()
+
+
+
+
+#im.show()
---
+++ texture.py
@@ -0,0 +1,20 @@
+from PIL import Image
+import numpy as np
+from math import floor
+
+class texture:
+    def __init__(self, colour):
+        self.colour = colour
+
+    def setTexture(self, texturePath):
+        im = Image.open(texturePath)
+        self.texture = np.asarray(im)
+        self.textureSize = (self.texture.shape[0]-1 ,self.texture.shape[1]-1)
+
+        
+    def getColour(self, xPos, yPos):
+        xind = floor((1-xPos) * self.textureSize[0])
+        yind = floor((yPos) * self.textureSize[1])
+
+        return self.texture[xind][yind]
+        #return (self.colour[0], self.colour[1]*xPos, self.colour[2]*yPos)
